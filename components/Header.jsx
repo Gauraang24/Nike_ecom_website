6207f188c2361f8e1122227eeafe1b9f6ee7aa08
gaurang.more <gaurang.more@sunday.tech>
2024-03-07 23:05:10 +0530

    Menu: Added logic for mobile menu and Web Menu Section

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import Wrapper from "./Wrapper";
 import Link from "next/link";
 import Menu from "./Menu"
+import MenuMobile from "./MenuMobile";
 
 //icons
 import { IoMdHeartEmpty } from 'react-icons/io'
@@ -27,12 +28,33 @@ const Header = () => {
                 setShowCatMenu={setShowCatMenu}
             />
 
-            <div className="flex items-center gap-2 text-black">
+            {mobileMenu && (<MenuMobile
+                showCatMenu={showCatMenu}
+                setShowCatMenu={setShowCatMenu}
+                setMobileMenu={setMobileMenu}
+            />)}
 
+            <div className="flex items-center gap-2 text-black">
+                {/* Icon Start */}
+                <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative">
+                    <IoMdHeartEmpty className="text-[19px] md:text=[24px]" />
+                    <div className="h-[14px] md:h-[18px] min-w-[14px] md:min-w-[18px] rounded-full bg-red-600 absolute top-1 left-5 md:left-7  text-white text-[10px] md:text-[12px] flex justify-center items-center px-[2px] md:px-[5px]">24</div>
+                </div>
+                {/* Icon End */}
+                {/* Icon Start */}
                 <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative">
                     <BsCart className="text-[15px] md:text=[20px]" />
-                    <div>5</div>
+                    <div className="h-[14px] md:h-[18px] min-w-[14px] md:min-w-[18px] rounded-full bg-red-600 absolute top-1 left-5 md:left-7  text-white text-[10px] md:text-[12px] flex justify-center items-center px-[2px] md:px-[5px]">5</div>
                 </div>
+                {/* Icon End */}
+
+                {/* {mobile Icon} */}
+                <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative -mr-2">{mobileMenu ? (
+                    <VscChromeClose className="text-[16px]" onClick={() => setMobileMenu(false)} />
+                ) : (
+                    <BiMenuAltRight className="text-[16px]" onClick={() => setMobileMenu(true)} />
+                )}</div>
+
             </div>
         </Wrapper>
     </header>;
